a214134422f63f6f199ec53f1de51d69efeaed7b
capricorncd <capricorncd@qq.com>
2022-07-23 10:10:04 +0900

    build: split Vendor Chunk

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -4,13 +4,13 @@
  * Date: 2022/06/11 09:54:35 (GMT+0900)
  */
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
   server: {
     host: '0.0.0.0',
     port: 9000,
@@ -18,5 +18,17 @@ export default defineConfig({
   build: {
     emptyOutDir: false,
     outDir: '../../demo',
+    // rollupOptions: {
+    //   // make sure to externalize deps that shouldn't be bundled
+    //   // into your library
+    //   external: ['vue'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue'
+    //     }
+    //   }
+    // }
   },
 })
